df84cb648e486cbb6ffcaef551c0bcb012454f21
gdevhun <wjh9330@naver.com>
2024-03-28 22:13:37 +0900

    FEAT:매니저 스크립트 추가, 폴더 정리

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] SpawnPoints;
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject Enemy = PoolManager.Instance.GetEnemy(PoolManager.EnemyType.Enemy1);
+        Enemy.gameObject.transform.SetPositionAndRotation(SpawnPoints[1].position, Quaternion.identity);
+    }
+}
