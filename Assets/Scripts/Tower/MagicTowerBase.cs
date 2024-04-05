830e4c1b117ea32fcf86978f5edd8a375e8f5ae7
LeeJungHwi <101587101+LeeJungHwi@users.noreply.github.com>
2024-04-05 18:30:48 +0900

    FEAT : 매직타워 스플래쉬 공격 이슈 1 : 타워 애니메이션과 타워 무기 발사 타이밍이 맞지않음 이슈 2 : 추후에 몬스터 스크립트에 접근하여 실제로 체력을 까야함

--- Assets/Scripts/Tower/MagicTowerBase.cs
+++ Assets/Scripts/Tower/MagicTowerBase.cs
@@ -6,17 +6,57 @@ public class MagicTowerBase : TowerBase
 {
     protected int splashDamage = 50; // 스플래쉬 데미지
 
-    // 스플래쉬 공격
+    // 타겟 공격
+    // 매직타워는 스플래쉬 공격
     protected override IEnumerator Attack()
     {
         while (isTarget)
         {
-            yield return new WaitForSeconds(attackSpeed); // 공격속도만큼 대기
+            // 공격속도만큼 대기
+            yield return new WaitForSeconds(attackSpeed);
 
-            // 타겟 적 주변 두명에게 스플래쉬 데미지
+            // 타워 무기 가져오기
+            GameObject towerWeapon = PoolManager.Instance.GetTowerWeapon(towerWeaponType);
+            Rigidbody2D towerWeaponRigid = towerWeapon.GetComponent<Rigidbody2D>();
+
+            // 위치 및 회전 초기화
+            towerWeapon.transform.position = transform.position;
+            towerWeapon.transform.rotation = towerWeapon.transform.rotation;
+
+            // 타워 무기 발사
+            Vector2 direction = (target.position - towerWeapon.transform.position).normalized;
+            towerWeaponRigid.velocity = direction * 5f;
+
+            // 무기 발사 각도
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+            towerWeapon.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            // 타겟팅된 몬스터 체력 감소
+            // target.GetComponent<Enemy>().health -= basicDamage;
             Debug.Log("단일 : " + target.name + ", 데미지 : " + basicDamage);
-            Debug.Log("스플래쉬 : " + target.name + ", 데미지 : " + splashDamage);
-            Debug.Log("스플래쉬 : " + target.name + ", 데미지 : " + splashDamage);
+
+            // 타겟팅된 몬스터 주변 두명 몬스터 스플래쉬 데미지
+            Collider2D[] hits = Physics2D.OverlapCircleAll(target.position, 1f);
+
+            int splashCnt = 0; // 스플래쉬 횟수
+
+            foreach (Collider2D hit in hits)
+            {
+                if (hit.CompareTag("Enemy") && splashCnt < 2 && hit.gameObject != target.gameObject)
+                {
+                    //hit.GetComponent<Enemy>().health -= splashDamage;
+                    Debug.Log("스플래쉬 : " + hit.gameObject.name + ", 데미지 : " + splashDamage);
+                    splashCnt++;
+                }
+            }
+
+            // 디버깅용
+            Debug.DrawLine(target.position + new Vector3(-1f, 0f, 0f), target.position + new Vector3(1f, 0f, 0f), Color.red, 2f);
+            Debug.DrawLine(target.position + new Vector3(0f, -1f, 0f), target.position + new Vector3(0f, 1f, 0f), Color.red, 2f);
+
+            // 1초 대기 후 타워 무기 비활성화
+            yield return new WaitForSeconds(1f);
+            towerWeapon.gameObject.SetActive(false);
         }
     }
 }
