6c159cf33dc35a58da0b5db01b8ff19054c20b46
gdevhun <wjh9330@naver.com>
2024-04-08 14:36:17 +0900

    Feat:골드매니저 스크립트 작성

---
+++ Assets/Scripts/Managers/GoldManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GoldManager : SingletonBehaviour<GoldManager>
+{
+    [SerializeField] private int playerGold;
+    [SerializeField] TextMeshProUGUI totalGold;
+    void Start()
+    {
+        playerGold = 0;
+        InitUIGold();
+    }
+
+    public void InitUIGold()
+    {
+        totalGold.text = playerGold.ToString();
+    }
+    //골드 획득 함수
+    public void AcquireGold(int amount)
+    {   //Enemy가 죽어서 비활성화시킬때 함수 호출하면됨.
+        playerGold += amount;
+        InitUIGold();
+    }
+
+    public void UseGold(int amount)
+    {
+        // 사용하려는 돈이 플레이어의 소지금보다 작거나 같은지 확인
+        if (amount <= playerGold)
+        {
+            playerGold -= amount; // 돈 사용
+
+        }
+        InitUIGold();
+        return;
+    }
+
+}
