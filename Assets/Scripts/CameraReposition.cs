b7c06e9f0c7bb9cd0f57b5d04d238835feaaa040
LeeJungHwi <101587101+LeeJungHwi@users.noreply.github.com>
2024-04-25 16:23:56 +0900

    Fix : 몬스터 스폰 렌더링

--- Assets/Scripts/CameraReposition.cs
+++ Assets/Scripts/CameraReposition.cs
@@ -5,15 +5,15 @@ public class CameraReposition : MonoBehaviour
 {
     public Sprite rightBtn;
     public Sprite leftBtn;
-    private readonly Vector3 _firstMapPos = Vector3.zero;
-    private readonly Vector3 _secondMapPos = new Vector3(80f, 0f, 0f);
+    private readonly Vector3 _firstMapPos = new Vector3(0f, 0f, -1f);
+    private readonly Vector3 _secondMapPos = new Vector3(80f, 0f, -1f);
     private bool _isFirstMap = true;
     private Vector3 _curCameraPos;
     
     // Start is called before the first frame update
     void Start()
     {
-        // ÃÊ±â À§Ä¡ ¼³Á¤
+        // ï¿½Ê±ï¿½ ï¿½ï¿½Ä¡ ï¿½ï¿½ï¿½ï¿½
         _curCameraPos = _isFirstMap ? _firstMapPos : _secondMapPos;
         Camera.main.transform.position = _curCameraPos;
     }
