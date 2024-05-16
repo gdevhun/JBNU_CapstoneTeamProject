0d3c44d091a05f914833a11ff1eb2ef1f020e714
gdevhun <wjh9330@naver.com>
2024-05-16 14:18:02 +0900

    refactor : unitask적용

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,94 +12,96 @@ public class EnemySpawner : SingletonBehaviour<EnemySpawner>
     public TextMeshProUGUI thisStageInfo;
     public GameObject stageTimerImage; 
     [SerializeField] private Transform[] spawnPoints;
-    private int _thisStageSpawnInterval; //���罺������ ��������
-    private int _thisStageEnableSpawnPt;  //���� �������� ����� ��������Ʈ
-    private int _thisStageEnemyNum; //���ݺ� ���׹� ��ȯ ��
+    private int _thisStageSpawnInterval; //해당 스테이지 스폰 간격
+    private int _thisStageEnableSpawnPt;  //해당 스테이지 가능한 스폰 포인트 수
+    private int _thisStageEnemyNum; //해당 스테이지 에네미 수
     private readonly string _stageName = "StageData";
     private int _thisStageNum = 1;
-    public bool isCurWaveEnded = true;
+    public bool isCurWaveEnded;
 
     private void Start()
     {
-        StartCoroutine(StartGame());
+        StartGame().Forget(); //게임시작하면 호출
     }
-
-    private IEnumerator StartGame()  //��ü���� ���� �ڷ�ƾ �Լ�
+    private async UniTaskVoid StartGame()
     {
-        while (_thisStageNum != 11)
+        while (_thisStageNum != 11) //임시 11스테이지까지
         {
-            StartCoroutine(CountDownAndSpawn()); //���̺����
-            yield return new WaitUntil(() => isCurWaveEnded); // ���� ���������� ����� ������ ���
-            _thisStageNum++;
+            isCurWaveEnded = false;
+            CountDownAndSpawn().Forget(); //10초 대기
+            await UniTask.WaitUntil(()=> isCurWaveEnded); //해당 웨이브가 끝날떄까지 대기
+            _thisStageNum++; //다음 스테이지로 (변수증가)
         }
-        Debug.Log("��");
     }
-    private void InitStageData()  //�������� ������ �ʱ�ȭ �Լ�
+    
+    private void InitStageData()  //해당 스테이지 데이터를 초기화
     {
-        StageManager.Instance.LoadStageData(_stageName + _thisStageNum);
+        StageManager.Instance.LoadStageData(_stageName + _thisStageNum); //스테이지정보 로드
         
-        if (!StageManager.Instance.isLoadedData) return; //�����ͷε� ����
+        if (!StageManager.Instance.isLoadedData) return; //데이터 로드 오류
+        thisStageInfo.text = StageManager.Instance.stageData.stageInfo; //HUD wave 업데이트
         
-        thisStageInfo.text = StageManager.Instance.stageData.stageInfo; //HUD wave �ؽ�Ʈ ����
-        
-        _thisStageSpawnInterval = StageManager.Instance.stageData.stageSpawnInteval; //���͹� �ε�
-        
-        _thisStageEnemyNum = StageManager.Instance.stageData.stageSpawnNum; //���� Ƚ�� �ε�
-        
-        _thisStageEnableSpawnPt = StageManager.Instance.stageData.stageEnableSpawnPt; //��������Ʈ ���
+        //각각 해당 스테이지에 대한 정보 업데이트
+        _thisStageSpawnInterval = StageManager.Instance.stageData.stageSpawnInteval; 
+        _thisStageEnemyNum = StageManager.Instance.stageData.stageSpawnNum; 
+        _thisStageEnableSpawnPt = StageManager.Instance.stageData.stageEnableSpawnPt; 
         
     }
 
-    private IEnumerator CountDownAndSpawn() //�긮ũ �� + ���̺���� �ڷ�ƾ
-    {
-        isCurWaveEnded = false;
+    private async UniTaskVoid CountDownAndSpawn()
+    {   
+        //10초를 대기하며 HUD 남은 초 수 업데이트
         nextStageIntervalSec.gameObject.SetActive(true);
         int cnt = 10;
         while (cnt > 0)
-        {   //10�ʿ� ���ļ� ī��Ʈ �ٿ��� ���ְ� �ؽ�Ʈ�� HUD�� ǥ������
-            yield return new WaitForSeconds(1f);
+        {   
+            await UniTask.Delay(TimeSpan.FromSeconds(1));
+
             cnt--;
             nextStageIntervalSec.text = cnt.ToString();
         }
         nextStageIntervalSec.text = "10";
-        yield return new WaitForSeconds(1f);
-        nextStageIntervalSec.gameObject.SetActive(false);
-        StartCoroutine(ActiveWaveStage()); //ī��Ʈ�� ������ ���� ����
-        yield return null;
-    }
-    
-    private IEnumerator ActiveWaveStage() //�������� �Լ�
-    {   //���� StageTime = �������͹� * ���� �� + 2 + [�߰� �ð�]
+        await UniTask.Delay(TimeSpan.FromSeconds(1));
         
-        InitStageData(); //���� ������ ���� ������ �ε�
-        stageTimerImage.SetActive(true);
+        nextStageIntervalSec.gameObject.SetActive(false);
+        
+        ActiveWaveStage().Forget(); //다음스테이지 활성화
+    }
+   
+    private async UniTaskVoid ActiveWaveStage()
+    {
+        InitStageData(); //해당 스테이지에대한 스크립터블 데이터 초기화
+        stageTimerImage.SetActive(true); //해당스테이지 타이머 작동
 
-        // 배경음
+        // 배경음 교체
         if(StageManager.Instance.stageData.enemyType == PoolManager.EnemyType.MiddleBoss) SoundManager.Instance.PlayBGM(SoundType.보스BGM3);
         else if(StageManager.Instance.stageData.enemyType == PoolManager.EnemyType.LastBoss) SoundManager.Instance.PlayBGM(SoundType.보스BGM2);
         else if(StageManager.Instance.stageData.enemyType == PoolManager.EnemyType.Enemy6) SoundManager.Instance.PlayBGM(SoundType.일반BGM);
         
-        for (int i = 0; i < _thisStageEnemyNum; i++)  //�� ���� Ƚ��
+        for (int i = 0; i < _thisStageEnemyNum; i++)  //해당스테이지 에네미넘만큼 반복 
         {
+            //해당스테이지에대해 가능한 스폰포인트수를 조사
             _thisStageEnableSpawnPt = StageManager.Instance.stageData.stageEnableSpawnPt;
             while (_thisStageEnableSpawnPt >= 0)
-            {   //���� ��������Ʈ�� ��ȯ. stageEnableSpawnPt=1 ���� ���ʷ� 4����.
+            {   
+                //각각스폰포인트수에 모두 스폰
                 SpawnEnemyInSp(_thisStageEnableSpawnPt);
                 _thisStageEnableSpawnPt--;
             }
-            //���͹� ��ŭ ��� �� �ٽ� ����
-            yield return new WaitForSeconds(_thisStageSpawnInterval);
+            //해당 스테이지 스폰간격만큼 대기 후 다시 반복
+            await UniTask.Delay(TimeSpan.FromSeconds(_thisStageSpawnInterval));
+            
         }
-        yield return new WaitForSeconds(2f);
-        yield return null;
+        //모두 스폰되면 2초 대기
+        await UniTask.Delay(TimeSpan.FromSeconds(2));
+        isCurWaveEnded = true;
     }
-    
     
     private void SpawnEnemyInSp(int sp)
     {
         Enemy enemy = PoolManager.Instance.GetEnemy(StageManager.Instance.stageData.enemyType).GetComponent<Enemy>();
         enemy.gameObject.transform.position = (spawnPoints[sp].position);
-        enemy.movePointNum = sp + 1; // �̵� ��� ����
+        enemy.movePointNum = sp + 1; 
         enemy.FirstMoveTarget();
 
         if(enemy.enemyGold == 2000 || enemy.enemyGold == 10000)
@@ -110,9 +113,4 @@ public class EnemySpawner : SingletonBehaviour<EnemySpawner>
             if(enemy.enemyGold == 10000) GameManager.Instance.lastBossImage.sprite = GameManager.Instance.lastBossSprite;
         }
     }
-
-    /*private void SpawnBoss()
-    {
-
-    }*/
 }
